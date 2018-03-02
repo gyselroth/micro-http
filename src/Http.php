6cd68e35506d39645c1a39f23b23631b3c1d737e
Raffael Sahli <sahli@gyselroth.com>
2018-03-02 18:02:52 +0100

    added 422 HTTP status

--- src/Http.php
+++ src/Http.php
@@ -58,6 +58,7 @@ class Http
         415 => 'Unsupported Media Type',
         416 => 'Requested Range Not Satisfiable',
         417 => 'Expectation Failed',
+        422 => 'Unprocessable Entity',
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
